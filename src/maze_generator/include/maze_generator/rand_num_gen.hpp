f48e937a57f5d2771e0a098e8f47086e04e96cbe
jacob-m-nash <jacob.matthew.nash@gmail.com>
2026-01-31 23:51:46 +0000

    start of sampler

--- src/maze_generator/include/maze_generator/rand_num_gen.hpp
+++ src/maze_generator/include/maze_generator/rand_num_gen.hpp
@@ -1,7 +1,18 @@
 #ifndef RAND_NUM_GEN_HPP
 #define RAND_NUM_GEN_HPP
+#include <iterator>
 class RandomNumberGenerator {
 public:
   int static random_int(int min, int max);
+
+  template <typename Container>
+  static auto sample(const Container &container) // TODO: use
+      -> decltype(*std::begin(container)) {
+    auto size = std::distance(std::begin(container), std::end(container));
+    auto index = randon_int(0, size - 1);
+    auto it = std::begin(container);
+    std::advance(it, index);
+    return *it;
+  }
 };
 #endif // !RAND_NUM_GEN_HPP
